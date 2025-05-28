0d5d3278eadf783823d5e42e958bced2b17417d9
Maria <mvb.mvidigal@gmail.com>
2025-05-28 14:03:43 +0200

    Mockups remesas || Finalizado

--- src/user/app/views/vControlAsistencia.php
+++ src/user/app/views/vControlAsistencia.php
@@ -51,45 +51,50 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 col-sm-12">
-
-                <?php if ($datos['esDiaLectivo'] && !empty($datos['alumnos'])): ?>
-                    <div class="text-center mb-4">
-                        <h3 class="d-inline-block bg-custom-secondary text-white px-4 py-2 rounded w-auto">
-                            <?php 
-                                $diasSemana = ['DOMINGO', 'LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO'];
-                                $meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
-                                $fecha = new DateTime();
-                                echo $diasSemana[$fecha->format('w')] . ' ' . $fecha->format('d') . ' DE ' . $meses[$fecha->format('n') - 1] . ' ' . $fecha->format('Y');
+                <?php
+                if ($datos['esDiaLectivo'] && !empty($datos['alumnos'])) {
+                ?>
+                <div class="text-center mb-4">
+                    <h3 class="d-inline-block bg-custom-secondary text-white px-4 py-2 rounded w-auto">
+                        <?php
+                        $diasSemana = ['DOMINGO', 'LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO'];
+                        $meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
+                        $fecha = new DateTime();
+                        echo $diasSemana[$fecha->format('w')] . ' ' . $fecha->format('d') . ' DE ' . $meses[$fecha->format('n') - 1] . ' ' . $fecha->format('Y');
+                        ?>
+                    </h3>
+                </div>
+                <!-- Tabla de asistencia -->
+                <div class="table-responsive">
+                    <table class="table table-sm table-asistencia">
+                        <thead>
+                            <tr>
+                                <th class="bg-custom-light pe-0 columna-nombre">NOMBRE DEL ALUMNO</th>
+                                <th class="bg-custom-light text-center ps-2 columna-asiste">ASISTE</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            foreach ($datos['alumnos'] as $alumno) {
+                                echo '<tr>';
+                                echo '<td class="py-1 pe-0 columna-nombre">' . htmlspecialchars($alumno['nombreAlumno']) . '</td>';
+                                echo '<td class="text-center py-1 ps-2 columna-asiste">';
+                                echo '<input type="checkbox" class="form-check-input control-asistencia"';
+                                echo ' data-id="' . $alumno['idAlumno'] . '"';
+                                echo in_array($alumno['idAlumno'], $datos['asistencias']) ? ' checked' : '';
+                                echo '>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
                             ?>
-                        </h3>
-                    </div>
-                    <!-- Tabla de asistencia -->
-                    <div class="table-responsive">
-                        <table class="table table-sm table-asistencia">
-                            <thead>
-                                <tr>
-                                    <th class="bg-custom-light pe-0 columna-nombre">NOMBRE DEL ALUMNO</th>
-                                    <th class="bg-custom-light text-center ps-2 columna-asiste">ASISTE</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($datos['alumnos'] as $alumno): ?>
-                                    <tr>
-                                        <td class="py-1 pe-0 columna-nombre"><?= htmlspecialchars($alumno['nombreAlumno']) ?></td>
-                                        <td class="text-center py-1 ps-2 columna-asiste">
-                                            <input type="checkbox" class="form-check-input control-asistencia"
-                                                data-id="<?= $alumno['idAlumno'] ?>"
-                                                <?= in_array($alumno['idAlumno'], $datos['asistencias']) ? 'checked' : '' ?>>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                <?php else: ?>
-                    <div class="alert alert-info text-center">No hay clases hoy</div>
-                <?php endif; ?>
-
+                        </tbody>
+                    </table>
+                </div>
+                    <?php
+                    } else {
+                        echo '<div class="alert alert-info text-center">Hoy no es un día lectivo</div>';
+                    }
+                    ?>
             </div>
         </div>
     </div>
